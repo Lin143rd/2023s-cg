c59dc330ecf9c4d3e2f4fab657afd4715f6421a9
Lin143rd <66264519+Lin143rd@users.noreply.github.com>
2023-05-01 22:17:33 +0900

    hoge

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const routerBaseName = process.env.PUBLIC_URL + "2023s-cg";
-
 root.render(
   <React.StrictMode>
-    <Router basename={routerBaseName}>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
 );
 
